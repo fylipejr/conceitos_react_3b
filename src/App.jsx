a216521b35af42451f0bc1fc74985010724afb2b
FYLIPE DENAZIO MELLO JÚNIOR <fylipe_mello-junior@estudante.sesisenai.org.br>
2026-04-28 12:12:36 -0300

    pi

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const renderizarTela = () =>{
+    // navegação
+  }
+
+  return (
+    <>
+      <section id="center">
+        
+      </section>
+
+    
+    </>
+  )
+}
+
+export default App
