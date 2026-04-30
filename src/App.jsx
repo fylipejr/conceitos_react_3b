7a7b156af87ca076dac74b1ce73b193d800a667b
FYLIPE DENAZIO MELLO JÚNIOR <fylipe_mello-junior@estudante.sesisenai.org.br>
2026-04-30 11:53:17 -0300

    pi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/login'
+import Cadastro from './paginas/Cadastro'
+import Usuario from './paginas/Usuario'
 
 function App() {
+const [tela, setTela] = useState()
+
   const renderizarTela = () =>{
-    // navegação
+   if(tela === 'login') {
+    return <Login/>
+   }else if (tela === 'cadastro') {
+    return <Cadastro/>
+   }
+   else if (tela === 'usuario')  {
+    return <Usuario/>
+   }
+  }
+  const trocarTela = (pagina) => {
+    setTela (pagina)
+
   }
 
-  return (
+    return (
     <>
-      <section id="center">
-        
-      </section>
-
+      <h1>pikachu</h1>
+      <button onClick={ () => trocarTela('login')}>Login</button>
+      <button onClick={ () => trocarTela('cadastro')}>Cadastro</button>
+      <button onClick={ () => trocarTela('Usuario')}>Usuario</button>
+      {renderizarTela()}
     
     </>
   )
---
+++ src/paginas/Usuario.jsx
@@ -0,0 +1,5 @@
+export default function Usuario () {
+        return (
+            <h1>Usuario</h1>
+        )
+    }
